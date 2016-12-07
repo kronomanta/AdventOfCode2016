3183c91da5362c0515057db698db62fb54bf5be9
Kanyó Tibor <kanyo.tibi@gmail.com>
2016-12-07 20:55:40 +0100

    day06 full day05 first half

---
+++ Day06/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace Day06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] columns = null;
+            foreach (string line in System.IO.File.ReadLines(@"..\..\Day06_input.txt"))
+            {
+                if (columns == null)
+                    columns = new string[line.Length];
+
+                for (int i = 0; i < line.Length; i++)
+                    columns[i] += line[i];
+            }
+
+            string decoded = "";
+            foreach (var column in columns)
+            {
+                decoded += column.GroupBy(x => x).OrderByDescending(x => x.Count()).Select(x => x.Key).FirstOrDefault();
+            }
+                        
+            System.Console.WriteLine(decoded);
+
+            decoded = "";
+            foreach (var column in columns)
+            {
+                decoded += column.GroupBy(x => x).OrderBy(x => x.Count()).Select(x => x.Key).FirstOrDefault();
+            }
+
+            System.Console.WriteLine(decoded);
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ Day07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //matches 'abba', not matches 'aaaa'
+            Regex patternToFind = new Regex(@"(.)((?!\1).)\2\1");
+            Regex patternNotToFind = new Regex(@"\[[^\]]*(.)((?!\1).)\2\1[^\[]*\]");
+
+            int counter = 0;
+            foreach (string line in System.IO.File.ReadLines(@"..\..\Day07_input.txt").Where(x => !patternNotToFind.IsMatch(x) && patternToFind.IsMatch(x)))
+            {
+                counter++;
+            }
+
+            System.Console.WriteLine(counter);
+
+            System.Console.ReadKey();
+        }
+    }
+}
