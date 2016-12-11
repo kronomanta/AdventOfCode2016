b725edfff83aec76be0f1c6f731df1b3d110eae2
Kanyó Tibor <kanyo.tibi@gmail.com>
2016-12-11 03:35:30 +0100

    Day07 part2

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Day07
@@ -8,39 +8,52 @@ namespace Day07
         static void Main(string[] args)
         {
             //matches 'abba', not matches 'aaaa'
-            //example 1
-            //Regex patternToFind = new Regex(@"(.)((?!\1).)\2\1");
-            //Regex patternNotToFind = new Regex(@"\[[^\]]*(.)((?!\1).)\2\1[^\[]*\]");
+            Regex patternToFind = new Regex(@"(.)((?!\1).)\2\1");
+            Regex patternNotToFind = new Regex(@"\[[^\]]*(.)((?!\1).)\2\1[^\[]*\]");
 
-            //example2
-            Regex patternToFind = new Regex(@"\]?[^\[^\]]*(.)((?!\1).)\1[^\[^\]]*\[?");
-            string squarePattern = @"\[[^\]]*{0}{1}{0}[^\[]*\]";
+            int sum1 = 0, sum2 = 0;
 
-
-            int counter = 0;
             foreach (string line in System.IO.File.ReadLines(@"..\..\Day07_input.txt"))
             {
-                MatchCollection matches = patternToFind.Matches(line);
-                if (matches.Count == 0) continue;
+                if (!patternNotToFind.IsMatch(line) && patternToFind.IsMatch(line))
+                    sum1++;
 
-                bool found = false;
-                foreach(Match match in matches)
-                {
-                    if (!new Regex(string.Format(squarePattern, match.Groups[1], match.Groups[2])).IsMatch(line) &&
-                        new Regex(string.Format(squarePattern, match.Groups[2], match.Groups[1])).IsMatch(line))
-                    {
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (found)
-                    counter++;
+                if (SupportSSL(line))
+                    sum2++;
             }
 
-            System.Console.WriteLine(counter);
+            System.Console.WriteLine(sum1);
+            System.Console.WriteLine(sum2);
 
             System.Console.ReadKey();
+        }
+        
+        private static bool SupportSSL(string line)
+        {
+            var segments = line.Split('[', ']');
+            for (int i = 0; i < segments.Length; i += 2)
+            {
+                foreach (string aba in GetABAs(segments[i]))
+                {
+                    //megnézem mindegyik illeszkedőre, ami kívül van, hogy tartozik-e hozzá belül is a megfordított
+                    for (int j = 1; j < segments.Length; j += 2)
+                    {
+                        if (segments[j].Contains(aba[1].ToString() + aba[0] + aba[1]))
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        static IEnumerable<string> GetABAs(string input)
+        {
+            for (int i = 0; i < input.Length - 2; i++)
+            {
+                if (input[i] == input[i + 2] && input[i] != input[i + 1])
+                    yield return (input[i].ToString() + input[i + 1] + input[i + 2]);
+            }
         }
     }
 }
